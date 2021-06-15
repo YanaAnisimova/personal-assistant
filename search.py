b5bcac051db63ecd26de3a49489c4e9abc329294
Delvirta <delvirta@gmail.com>
2021-06-15 16:38:57 +0300

    added search funcs

--- search.py
+++ search.py
@@ -9,8 +9,8 @@ def users_to_congratulate(n):
     with open('data//data-file.txt', 'a') as file:
         users = file.readlines()
         for user in users:
-            if re.search(search_pattern, re.split(' ',user)[3]):
-                user_list.append(re.split(' ', user)[0].strip()) 
+            if re.search(search_pattern, re.split(',',user)[3]):
+                user_list.append(re.split(',', user)[0].strip()) 
     result = '\n'.join(user_list)       
     return f'Please do not forget to tell them happy birthday!\n{result}'
 
@@ -19,8 +19,8 @@ def user_search(user_name):
     with open('data//data-file.txt', 'a') as file:
         users = file.readlines()
         for user in users:
-            if user_name in re.split(' ', user)[0]:
-                user_data = re.split(' ', user)[0].strip() + re.split(' ', user)[1].strip()
+            if user_name in re.split(',', user)[0]:
+                user_data = re.split(',', user)[0].strip() + re.split(',', user)[1].strip()
                 user_list.append(user_data)
     result = '\n'.join(user_list)
     return f'Found some matches:\n{result}'
@@ -30,8 +30,8 @@ def note_search(key_word):
     with open('data//data-file.txt', 'a') as file:
         users = file.readlines()
         for user in users:
-            if key_word in re.split(' ', user)[-1]:
-                user_data = re.split(' ', user)[0].strip() + re.split(' ', user)[-1].strip()
+            if key_word in re.split(',', user)[-1]:
+                user_data = re.split(',', user)[0].strip() + re.split(',', user)[-1].strip()
                 user_list.append(user_data)
     result = '\n'.join(user_list)
     return f'Found some users with matching notes:\n{result}'
